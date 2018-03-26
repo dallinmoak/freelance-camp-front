ecf117c4bc536bb2086e17098fc48aab03260acf
dallinmoak <dallinmoak@gmail.com>
2018-03-26 11:52:36 -0600

    mocked up proposal data

--- app/proposal/proposal-list.component.ts
+++ app/proposal/proposal-list.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import{Proposal} from './proposal';
 
 @Component({
   moduleId: module.id,
@@ -6,4 +7,40 @@ import {Component} from '@angular/core';
   templateUrl: 'proposal-list.component.html'
 })
 
-export class ProposalListComponent{}
+export class ProposalListComponent{
+  proposalOne: Proposal = new Proposal(
+    15,
+    "ABC Company",
+    "http://devcamp.com",
+    "Rails",
+    150,
+    120,
+    15,
+    "a@b.c"
+  )
+  proposalTwo: Proposal = new Proposal(
+    16,
+    "xyz Company",
+    "http://devcamp.com",
+    "Rails",
+    150,
+    120,
+    15,
+    "a@b.c"
+  )
+  proposalThree: Proposal = new Proposal(
+    17,
+    "something Company",
+    "http://devcamp.com",
+    "Rails",
+    150,
+    120,
+    15,
+    "a@b.c"
+  )
+  proposals: Proposal[] = [
+    this.proposalOne,
+    this.proposalTwo,
+    this.proposalThree
+  ]
+}
