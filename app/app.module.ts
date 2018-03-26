39be02661273f7c41c57d73c9e2b943124ab8346
dallinmoak <dallinmoak@gmail.com>
2018-03-26 11:02:36 -0600

    propsal component made and added to routing system

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,7 +3,8 @@ import {BrowserModule}  from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
 import {HomepageComponent} from './homepage/homepage.component';
-import { DocumentsComponent } from './Documents/documents.component';
+import {DocumentsComponent} from './Documents/documents.component';
+import {ProposalListComponent} from './proposal/proposal-list.component';
 
 import {AppRoutingModule} from './app-routing.module';
 
@@ -15,7 +16,8 @@ import {AppRoutingModule} from './app-routing.module';
   declarations: [
     AppComponent,
     HomepageComponent,
-    DocumentsComponent
+    DocumentsComponent,
+    ProposalListComponent
   ],
   bootstrap: [
     AppComponent
